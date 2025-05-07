27d36026ea4dce77b7b9496ea69233d50c17f8e7
Clara Helena Alencar Sena <clarahelenasena@gmail.com>
2025-05-07 12:58:29 -0400

    update: add cardConsulta component

---
+++ frontend/src/app/components/cards/CardConsultas.tsx
@@ -0,0 +1,51 @@
+import { LuList } from 'react-icons/lu';
+import { HiClock, HiDotsVertical } from 'react-icons/hi';
+import { FaUser } from 'react-icons/fa';
+
+interface ConsultaData {
+  diaSemana: string;
+  diaMes: string;
+}
+
+interface Consulta {
+  id: number;
+  data: ConsultaData;
+  tratamento: string;
+  horário: string;
+  paciente: string;
+}
+
+interface CardConsultaProps {
+  item: Consulta;
+}
+
+export default function CardConsulta({ item }: CardConsultaProps) {
+  return (
+    <section className='bg-[#F1EDE3] justify-between w-full flex rounded-lg p-4 '>
+      <div className='flex md:gap-8'>
+        <div className='flex flex-col items-center justify-center pr-4 border-r-2 border-[#6B4A2E]/40 sm:pl-3 sm:pr-5 md:pl-8 md:pr-10 lg:justify-center '>
+        <p className='font-semibold text-lg text-[#1f1005] md:font-semibold md:text-center'>{item.data.diaSemana}</p>
+        <p className='font-bold text-4xl text-[#6A5242] md:text-center'>{item.data.diaMes}</p>
+      </div>
+      <div className='flex flex-col px-2'>
+        <div className='flex items-center gap-2'>
+          <LuList className='text-[#6A5242]'/> 
+          <p className='line-clamp-1'>{item.tratamento}</p>
+        </div>
+        <div className='flex items-center gap-2'>
+          <HiClock className='text-[#6A5242]'/> 
+          <p className='line-clamp-1'>{item.horário}</p>
+        </div>
+        <div className='flex items-center gap-2'>
+          <FaUser  className='text-[#6A5242]'/> 
+          <p className='line-clamp-1'>{item.paciente}</p>
+        </div>
+      </div>
+      </div>
+      <div className='flex flex-col md:justify-center'>         
+        <button className='md:hidden'><HiDotsVertical/></button>
+        <button className='hidden md:flex bg-[#6A5242] text-white hover:cursor-pointer px-8 py-1 rounded-lg text-sm'>Editar</button>
+      </div>
+    </section>
+  )
+}
--- frontend/src/app/consultas/page.tsx
+++ frontend/src/app/consultas/page.tsx
@@ -4,6 +4,7 @@ import { FaUser } from 'react-icons/fa6';
 import { HiDotsVertical } from 'react-icons/hi';
 import { HiClock } from 'react-icons/hi2';
 import { LuList } from 'react-icons/lu';
+import CardConsulta from './../components/cards/CardConsultas';
 
 
 export default function Consultas() {
@@ -24,7 +25,9 @@ export default function Consultas() {
   const consultas = [
     {id: 1, tratamento: "Liberação miofascials gsg", horário: "09:30 - 10:30", paciente: "Andressa Andrade", data: {diaMes: "20", diaSemana: "Seg"}, status: "concluida"},
     {id: 2, tratamento: "Liberação miofascial", horário: "11:30 - 12:30", paciente: "Ana Andrade", data: {diaMes: "20", diaSemana: "Seg"}, status: "concluida"},
-    {id: 3, tratamento: "Liberação miofascial", horário: "14:30 - 15:30", paciente: "Ana Frotasgsgsg ggsgsgs", data: {diaMes: "20", diaSemana: "Seg"}, status: "concluida"}
+    {id: 3, tratamento: "Liberação miofascial", horário: "14:30 - 15:30", paciente: "Ana Frotasgsgsg ggsgsgs", data: {diaMes: "20", diaSemana: "Seg"}, status: "concluida"},
+    {id: 4, tratamento: "Liberação miofascial", horário: "11:30 - 12:30", paciente: "Ana Andrade", data: {diaMes: "20", diaSemana: "Seg"}, status: "concluida"},
+    {id: 5, tratamento: "Liberação miofascial", horário: "14:30 - 15:30", paciente: "Ana Frotasgsgsg ggsgsgs", data: {diaMes: "20", diaSemana: "Seg"}, status: "concluida"}
   ]
   return (
     <div className='w-full'>
@@ -80,38 +83,14 @@ export default function Consultas() {
             </button>
           ))}
           </div>
-
+          
+          {/* arrumar um jeito de exibir consultas especificas po dia e por status */}
           {/*Seção das consultas do dia*/}
           <section className='flex flex-col gap-3 py-4 px-6 w-full'>
             <p className='text-lg font-medium'>Para hoje</p>
             <div className='flex flex-col gap-4'>
             {consultas.map((item) => (
-              <section className='bg-[#F1EDE3] justify-between w-full flex rounded-lg p-4 '>
-                <div className='flex md:gap-8'>
-                  <div className='flex flex-col items-center justify-center pr-4 border-r-2 border-[#6B4A2E]/40 sm:pl-3 sm:pr-5 md:pl-8 md:pr-10 lg:justify-center '>
-                  <p className='font-semibold text-lg text-[#1f1005] md:font-semibold md:text-center'>{item.data.diaSemana}</p>
-                  <p className='font-bold text-4xl text-[#6A5242] md:text-center'>{item.data.diaMes}</p>
-                </div>
-                <div className='flex flex-col px-2'>
-                  <div className='flex items-center gap-2'>
-                    <LuList className='text-[#6A5242]'/> 
-                    <p className='line-clamp-1'>{item.tratamento}</p>
-                  </div>
-                  <div className='flex items-center gap-2'>
-                    <HiClock className='text-[#6A5242]'/> 
-                    <p className='line-clamp-1'>{item.horário}</p>
-                  </div>
-                  <div className='flex items-center gap-2'>
-                    <FaUser  className='text-[#6A5242]'/> 
-                    <p className='line-clamp-1'>{item.paciente}</p>
-                  </div>
-                </div>
-                </div>
-                <div className='flex flex-col md:justify-center'>         
-                  <button className='md:hidden'><HiDotsVertical/></button>
-                  <button className='hidden md:flex bg-[#6A5242] text-white hover:cursor-pointer px-8 py-1 rounded-lg text-sm'>Editar</button>
-                </div>
-              </section>
+                <CardConsulta key={item.id} item={item} />
               ))}
             </div>
           </section>
@@ -121,33 +100,8 @@ export default function Consultas() {
           <section className='flex flex-col gap-3 py-4 px-6 w-full'>
             <p className='text-lg font-medium'>Para esta semana</p>
             <div className='flex flex-col gap-4'>
-            {consultas.map((item) => (
-              <section className='bg-[#F1EDE3] justify-between w-full flex rounded-lg p-4 '>
-                <div className='flex md:gap-8'>
-                  <div className='flex flex-col items-center justify-center pr-4 border-r-2 border-[#6B4A2E]/40 sm:pl-3 sm:pr-5 md:pl-8 md:pr-10 lg:justify-center '>
-                  <p className='font-semibold text-lg text-[#1f1005] md:font-semibold md:text-center'>{item.data.diaSemana}</p>
-                  <p className='font-bold text-4xl text-[#6A5242] md:text-center'>{item.data.diaMes}</p>
-                </div>
-                <div className='flex flex-col px-2'>
-                  <div className='flex items-center gap-2'>
-                    <LuList className='text-[#6A5242]'/> 
-                    <p className='line-clamp-1'>{item.tratamento}</p>
-                  </div>
-                  <div className='flex items-center gap-2'>
-                    <HiClock className='text-[#6A5242]'/> 
-                    <p className='line-clamp-1'>{item.horário}</p>
-                  </div>
-                  <div className='flex items-center gap-2'>
-                    <FaUser  className='text-[#6A5242]'/> 
-                    <p className='line-clamp-1'>{item.paciente}</p>
-                  </div>
-                </div>
-                </div>
-                <div className='flex flex-col md:justify-center'>         
-                  <button className='md:hidden'><HiDotsVertical/></button>
-                  <button className='hidden md:flex bg-[#6A5242] text-white hover:cursor-pointer px-8 py-1 rounded-lg text-sm'>Editar</button>
-                </div>
-              </section>
+              {consultas.map((item) => (
+                <CardConsulta key={item.id} item={item} />
               ))}
             </div>
           </section>
