f7a982ee781f268f85a113e9f9a1b035c7f52e6e
Clara Helena Alencar Sena <clarahelenasena@gmail.com>
2025-05-16 13:33:18 -0400

    feat: CreateTratamentoDialog component

--- frontend/src/components/ErrorMessage/index.tsx
+++ frontend/src/components/ErrorMessage/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 export function ErrorMessage({ refetch, isLoading, className }: { refetch: () => void, isLoading?: boolean, className?: string }) {
   return (
-    <>
+    <div className="py-4 flex flex-col items-center gap-1" >
       <h3 className={'text-xl lg:text-2xl text-center font-medium ' + className}>Ocorreu um erro ao carregar consultas</h3>
       <p className={"text-center " + className}>Tente novamente mais tarde!</p>
       <button onClick={() => refetch()} className="flex text-white justify-center items-center bg-primary mt-2 h-8 w-40 rounded-md cursor-pointer">
@@ -14,6 +14,6 @@ export function ErrorMessage({ refetch, isLoading, className }: { refetch: () =>
           : 'Recarregar'
         }
         </button>
-    </>
+    </div>
   )
 }
