46cfe2d705cf81788c3e96339ede341f4fca4e75
Leonardo3737 <leovinicius3737@gmail.com>
2025-05-20 23:27:29 -0400

    adicionar indicador de que ha consulta nos dias do calendario mobile

--- frontend/src/components/scheduler/index.tsx
+++ frontend/src/components/scheduler/index.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import { getDaysInMonth } from "@/utils/getDaysInMonth";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { CalendarDay } from "./calendarDay";
 import { isSameDay } from "@/utils/isSameDay";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { StatusView } from "@/constants/StatusView";
 import { StatusColor } from "@/constants/StatusColor";
-import { AppointmentrMobile } from "./mobile";
+import { AppointmentMobile } from "./mobile";
 import { useAppointment } from "@/hooks/useAppointment";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -27,14 +28,12 @@ const weekDays = [
 export const Scheduler = () => {
   const [currentMonth, setCurrentMonth] = useState(new Date());
 
-  const appointment = useAppointment()
-
+  const isMobile = useIsMobile()
 
   const mounth = useCallback(() => currentMonth.toLocaleDateString('pt-BR', {
     month: 'long',
   }), [currentMonth])
 
-  const days = useCallback(() => getDaysInMonth(currentMonth), [currentMonth])  // Função utilitária
 
   const nextMonth = () => setCurrentMonth(current => {
     const aux = new Date(current)
@@ -58,45 +57,80 @@ export const Scheduler = () => {
         </div>
         <span className="flex items-center mr-2 font-medium">{currentMonth.toLocaleDateString('pt-br', { year: 'numeric' })}</span>
       </div>
-      <div className="hidden lg:block">
-        <div className="grid grid-cols-7  grid-rows-1 gap-[1px] rounded-t-xl bg-[#D4D0C6] border-b border-[#D4D0C6] overflow-hidden w-full">
-          {weekDays.map((day, index) => (
-            <p key={`WeekDay-${index}`} className="text-center font-medium bg-[#F6F5F2] h-full py-2 text-[#6A5242]">{day}</p>
-          ))}
-        </div>
-        <div className="grid grid-cols-7 grid-rows-5 gap-[1px] w-full bg-[#D4D0C6] rounded-b-xl overflow-hidden">
-          {
-            !appointment.isPending && !appointment.isError ?
-              days().map((date, index) => (
-                <CalendarDay
-                  key={`CalendarDay-${index}`}
-                  date={date.date}
-                  isCurrentMonth={date.isCurrentMonth}
-                  events={appointment.data?.filter(e => isSameDay(e.date, date.date)) || []}
-                  index={index}
-                />
-              )) : <></>
-          }
-        </div>
-        <div className="flex justify-center gap-5 mt-2">
-          {
-            Object.keys(StatusView).map((status, index) =>
-              <div key={`legendStatusColors=${index}`} className="flex items-center">
-                <span className={`w-3 h-3 block rounded-full mr-2 bg-${StatusColor[status as keyof typeof StatusColor]}`}></span>
-
-                {StatusView[status as keyof typeof StatusView]}
-              </div>
-            )
-          }
-        </div>
-      </div>
-      <div className="block lg:hidden mt-4">
-        <AppointmentrMobile
-          month={currentMonth}
-          setMonth={setCurrentMonth}
-        />
-      </div>
+      {
+        isMobile ?
+          <AppointmentMobile
+            month={currentMonth}
+            setMonth={setCurrentMonth}
+          /> :
+          <Calendar currentMonth={currentMonth} />
+      }
     </>
 
   );
 };
+
+function Calendar({ currentMonth }: { currentMonth: Date }) {
+
+  const days = useMemo(() => getDaysInMonth(currentMonth), [currentMonth])  // Função utilitária
+
+  const rangeMonth = useMemo(() => {
+    const monthDays = days
+
+    console.log('pc');
+
+
+    const initial = new Date(monthDays[0].date)
+    initial.setHours(0, 0, 0, 0)
+
+    const final = new Date(monthDays[monthDays.length - 1].date)
+    final.setHours(23, 59, 59, 999)
+
+
+    return {
+      final,
+      initial
+    }
+  }, [days])
+
+  const appointment = useAppointment({
+    initialDate: rangeMonth.initial,
+    finalDate: rangeMonth.final
+  })
+
+
+  return (
+    <>
+      <div className="grid grid-cols-7  grid-rows-1 gap-[1px] rounded-t-xl bg-[#D4D0C6] border-b border-[#D4D0C6] overflow-hidden w-full">
+        {weekDays.map((day, index) => (
+          <p key={`WeekDay-${index}`} className="text-center font-medium bg-[#F6F5F2] h-full py-2 text-[#6A5242]">{day}</p>
+        ))}
+      </div>
+      <div className="grid grid-cols-7 grid-rows-5 gap-[1px] w-full bg-[#D4D0C6] rounded-b-xl overflow-hidden">
+        {
+          !appointment.isPending && !appointment.isError ?
+            days.map((date, index) => (
+              <CalendarDay
+                key={`CalendarDay-${index}`}
+                date={date.date}
+                isCurrentMonth={date.isCurrentMonth}
+                events={appointment.data?.filter(e => isSameDay(e.date, date.date)) || []}
+                index={index}
+              />
+            )) : <></>
+        }
+      </div>
+      <div className="flex justify-center gap-5 mt-2">
+        {
+          Object.keys(StatusView).map((status, index) =>
+            <div key={`legendStatusColors=${index}`} className="flex items-center">
+              <span className={`w-3 h-3 block rounded-full mr-2 bg-${StatusColor[status as keyof typeof StatusColor]}`}></span>
+
+              {StatusView[status as keyof typeof StatusView]}
+            </div>
+          )
+        }
+      </div>
+    </>
+  )
+}
