96504b43c25f8d4637c21aed38a3598e8acef01d
Clara Helena <106986725+clarasenx@users.noreply.github.com>
2025-05-28 16:35:11 -0400

    update: notas page.tsx

--- frontend/src/app/(private)/consultas/[id]/page.tsx
+++ frontend/src/app/(private)/consultas/[id]/page.tsx
@@ -5,31 +5,42 @@ export default async function Nota({
 }) {
   const { id } = await params
   return (
-    <div className='px-4 sm:px-8 pt-4'>
-      <section className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6'>
-        <div className='w-full h-full flex flex-col items-center gap-2'>
-          <p className='font-semibold text-nowrap sm:text-lg'>Paciente:</p>
-          <p>paciente x</p>
+    <div className='w-full h-full px-4 sm:px-8 pt-4'>
+      <section className='border-x rounded-t-lg '>
+        <div className='flex flex-col sm:flex-row w-full h-full items-center bg-[#9C7C5A] rounded-t-lg border-t py-2 px-4'>
+          <p className='sm:text-lg lg:text-xl font-semibold text-white'>Paciente x</p>
         </div>
-        <div className='w-full h-full gap-2 justify-center'>
-          <p className='font-semibold text-nowrap sm:text-lg'>Tratamento:</p>
-          <p>tratamento x</p>
-        </div>
-        <div className='w-full h-full gap-2 '>
-          <p className='font-semibold text-nowrap sm:text-lg'>Data da consulta:</p>
-          <p>10/05/25, 15:00</p>
-        </div>
-        <div className='w-full h-full gap-2'>
-          <p className='font-semibold text-nowrap sm:text-lg'>Telefone:</p>
-          <p>00000-0000</p>
-        </div>
-        <div className='w-full h-full gap-2 '>
-          <p className='font-semibold text-nowrap sm:text-lg'>Status:</p>
-          <p>Agendada</p>
-        </div>
+
+        <section className='bg-white px-4 py-2 grid xs:grid-cols-2 md:grid-cols-4 gap-1 md:gap-2'>
+          <div className='flex flex-col sm:flex-row w-full h-full sm:gap-2 items-center col-span-1 md:col-span-2'>
+            <p className='font-medium text-nowrap sm:text-lg'>Tratamento:</p>
+            <p>tratamento x</p>
+          </div>
+
+          <div className='flex flex-col sm:flex-row w-full h-full sm:gap-2  items-center col-span-1 md:col-span-2'>
+            <p className='font-medium sm:text-lg'>Data da consulta:</p>
+            <p>10/05/25, 15:00</p>
+          </div>
+
+          <div className='flex flex-col sm:flex-row w-full h-full sm:gap-2  items-center col-span-1 md:col-span-2'>
+            <p className='font-medium sm:text-lg'>Telefone:</p>
+            <p>00000-0000</p>
+          </div>
+
+          <div className='flex flex-col sm:flex-row w-full h-full sm:gap-2 items-center col-span-1 md:col-span-2'>
+            <p className='font-medium text-nowrap sm:text-lg'>Status:</p>
+            <p>Agendada</p>
+          </div>
+        </section>
       </section>
-      <section className='bg-white w-full h-full min-h-[200px] sm:min-h-[300px] lg:min-h-[700px] rounded-lg py-1.5 sm:py-5 px-2 sm:px-4 shadow'>
-        <textarea name="" id="" className='w-full h-full'></textarea>
+
+      <section 
+        className='flex bg-white w-full h-full max-h-4/5 rounded-b-lg sm:py-5 p-2 sm:px-4 border'>
+        <textarea 
+          name="" 
+          id=""
+          placeholder='Escreva suas anotações aqui!'
+          className='w-full min-h-[300px] '></textarea>
       </section>
     </div>
   )
